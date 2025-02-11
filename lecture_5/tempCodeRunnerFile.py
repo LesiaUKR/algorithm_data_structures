8984aa8e0dd008768a134c58cf32c62aa3d98401
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-02-11 14:28:07 +0000

    feat: add Chapter_1 for advanced_algorithms

--- lecture_5/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-def linear_search(arr, x): # arr - масив\список, x - елемент, який потрібно знайти
-   for i in range(len(arr)): # проходимо по кожному елементу масиву
-      if arr[i] == x: # якщо елемент знайдено
-         return i # повертаємо його індекс
-   return -1 # якщо елемент не знайдено повертаємо -1
-
-numbers = [1, 3, 5, 7, 9, 11]
-print(linear_search(numbers, 7))  # Output: 3
